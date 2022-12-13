7fcd6f1843740ede02719f2efdc2b77cbeca0c25
Aaron Hung <ahungwork@yahoo.ca>
2022-12-12 21:46:04 -0800

    add url, views and logic for dog detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
+from .models import Dog
 # from .models import Dog
 # from django.http import HttpResponse
 
 # Add the Dog class & list and view function below the imports
-class Dog: # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Dog: # Note that parens are optional if not inheriting from another class
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-dogs = [
-    Dog('Mango', 'Golden Retriever', 'Scottish breed of retriever dog of medium size', 5), 
-    Dog('Yeti', 'Husky', 'Medium-sized working sled dog breed', 3), 
-    Dog('Ranger', 'Rough Collie', 'Sable and white, where the "sable" ranges from pale tan to a mahogany', 3), 
-    Dog('Quicksilver', 'German Shepherd', 'Domed forehead, a long square-cut muzzle with strong jaws and a black nose', 4)
-]
+# dogs = [
+#     Dog('Mango', 'Golden Retriever', 'Scottish breed of retriever dog of medium size', 5), 
+#     Dog('Yeti', 'Husky', 'Medium-sized working sled dog breed', 3), 
+#     Dog('Ranger', 'Rough Collie', 'Sable and white, where the "sable" ranges from pale tan to a mahogany', 3), 
+#     Dog('Quicksilver', 'German Shepherd', 'Domed forehead, a long square-cut muzzle with strong jaws and a black nose', 4)
+# ]
+
 
 # Create your views here.
 def home(request):
@@ -26,4 +28,10 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { "dogs": dogs })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog':dog })
+
