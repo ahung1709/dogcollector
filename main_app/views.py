a2d598f6cdc2fa4f32b6f80acad02b61be5aec27
Aaron Hung <ahungwork@yahoo.ca>
2022-12-14 00:36:17 -0800

    update comments for base and views

--- main_app/views.py
+++ main_app/views.py
@@ -2,23 +2,6 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 from .forms import FeedingForm
-# from .models import Dog
-# from django.http import HttpResponse
-
-# Add the Dog class & list and view function below the imports
-# class Dog: # Note that parens are optional if not inheriting from another class
-#     def __init__(self, name, breed, description, age):
-#         self.name = name
-#         self.breed = breed
-#         self.description = description
-#         self.age = age
-
-# dogs = [
-#     Dog('Mango', 'Golden Retriever', 'Scottish breed of retriever dog of medium size', 5), 
-#     Dog('Yeti', 'Husky', 'Medium-sized working sled dog breed', 3), 
-#     Dog('Ranger', 'Rough Collie', 'Sable and white, where the "sable" ranges from pale tan to a mahogany', 3), 
-#     Dog('Quicksilver', 'German Shepherd', 'Domed forehead, a long square-cut muzzle with strong jaws and a black nose', 4)
-# ]
 
 # Create your views here.
 
